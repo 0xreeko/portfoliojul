f38124bbe6a8808aacff328ee936e4d840b7726d
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-07-05 18:49:38 +0100

    added deps, Footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+ 
+export const Footer = () => {
+    return (
+        <div className={styles.container}>
+            <p>&copy; 0xreeko</p>
+            <p>Made with 👾 by 0xreeko</p>
+            <p>Built with NextJs and TailwindCSS</p>
+        </div>
+    )
+};
---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+const About: NextPage = () => {
+    return (
+        <div className='h-screen'>this is a page</div>
+    )
+};
+
+export default About
