83f6d328473728c06160a1c964011205b6f2d1e2
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-03 11:21:27 +0100

    added link for RUI in Alert

--- components/Alert/Alert.tsx
+++ components/Alert/Alert.tsx
@@ -2,8 +2,8 @@ import styles from './Alert.module.css'
  
 export const Alert = () => {
     return (
-        <div className='flex justify-center px-8 py-2 mx-auto font-medium text-center  bg-stratos-900/20 backdrop-blur-sm sm:px-16 md:px-24 text-sylver-100'>
-            <p>Say hello to <span className="underline text-kunzite-400 underline-offset-2">RuskelUI</span>, a set of UI components for rapid Web3 development! Track progress via <a href="https://0xreeko.github.io/ruskelui" target={`_blank`} className='inline-flex items-center gap-1 text-pink-400 duration-300 hover:text-pink-400/70'><span className='italic'>Storybook</span><svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" /></svg></a></p>
+        <div className='flex justify-center px-8 py-2 mx-auto font-medium text-center bg-stratos-900/20 backdrop-blur-sm sm:px-16 md:px-24 text-sylver-100'>
+            <p>Say hello to <a href="https://www.npmjs.com/package/ruskelui" target={`_blank`} className={'text-kunzite-400 hover:text-kunzite-400/70 duration-200 inline-flex italic items-center gap-1'}><span className="underline underline-offset-2">RuskelUI</span><svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" /></svg></a>, a set of UI components for rapid Web3 development! Track progress via <a href="https://0xreeko.github.io/ruskelui" target={`_blank`} className='inline-flex items-center gap-1 text-pink-400 duration-200 hover:text-pink-400/70'><span className='italic'>Storybook</span><svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" /></svg></a></p>
         </div>
     )
 };
