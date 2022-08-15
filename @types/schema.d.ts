19e5805a2581cbf0b08f3b5b84fff48dc68403bd
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-15 09:15:44 +0100

    added types dir, modded config

---
+++ @types/schema.d.ts
@@ -0,0 +1,16 @@
+export type Tag = {
+    color: string,
+    id: string,
+    name: string
+}
+
+export type ReekoPost = {
+    id: string,
+    title: string,
+    slug: string,
+    tags: Tag[],
+    description: string,
+    published: boolean,
+    date: string
+    author: string
+}
--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-})
-
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = (withMDX({
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-})), (nextConfig)
+module.exports = (nextConfig)
