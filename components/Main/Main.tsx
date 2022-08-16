2aa4d9527c45d34165e43a0c4dd02594b352349e
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-16 10:29:13 +0100

    renamed the gsap icon to avoid a clash in the index page, removed About module -> useless now & relocated the styles for each skill molecule to the Main module

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { RuiButton } from 'ruskelui';
 import {gsap} from 'gsap'
 import { useEffect, useRef } from 'react';
+import { ethers, githubActions, gql, greensock, hardhat, jwt, mongodb, nextjs, node, photoshop, python, solitidy, tailwind, typescript } from '../Icons';
 
 export const Main = () => {
     let punkLogo = useRef(null)
@@ -42,7 +43,70 @@ export const Main = () => {
                 <Link href={'/about'}><RuiButton size='base' color='amethyst'>More about me</RuiButton></Link>
                 <Link href={'/blog'} className={`border-2`}><RuiButton size='base' color='russian'>Read the blog</RuiButton></Link>
             </div>
-            
+            <div className="py-12">
+                        <h2 className="text-m-h2 sm:text-d-h2"></h2>
+                        <div className="grid grid-cols-2 gap-4 mt-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-y-8 justify-items-center">
+                            <div className={styles.mainSkillItem}>
+                                <span>{nextjs}</span>
+                                <p>NextJS</p>
+                            </div>
+                            <div className={styles.mainSkillItem}>
+                                <span>{typescript}</span>
+                                <p>Typescript</p>
+                            </div>
+                            <div className={styles.mainSkillItem}>
+                                <span>{tailwind}</span>
+                                <p>Tailwind</p>
+                            </div>
+                            <div className={styles.mainSkillItem}>
+                                <span>{node}</span>
+                                <p>Node & Express</p>
+                            </div>
+
+                            <div className={styles.mainSkillItem}>
+                                <span>{gql}</span>
+                                <p>GraphQL</p>
+                            </div>
+                            <div className={styles.mainSkillItem}>
+                                <span>{jwt}</span>
+                                <p>JWT</p>
+                            </div>
+                            <div className={`${styles.mainSkillItem} relative`}>
+                                <span>{greensock}</span>
+                                <p>GSAP</p>
+                            </div>
+                            <div className={styles.mainSkillItem}>
+                                <span>{githubActions}</span>
+                                <p>GitHub Actions</p>
+                            </div>
+                            <div className={styles.mainSkillItem}>
+                                <span>{python}</span>
+                                <p>Python</p>
+                            </div>
+                            <div className={styles.mainSkillItem}>
+                                {/* <SiEthereum className='duration-300 w-14 h-14 fill-azure-500' /> */}
+                                <span>{ethers}</span>
+                                <p>EthersJS</p>
+                            </div>
+                            <div className={styles.mainSkillItem}>
+                                {/* <FaHardHat className='duration-300 w-14 h-14 fill-jasper-500' /> */}
+                                <span>{ hardhat}</span>
+                                <p>Hardhat</p>
+                            </div>
+                            <div className={styles.mainSkillItem}>
+                                {solitidy}
+                                <p>Solidity</p>
+                            </div>
+                            <div className={styles.mainSkillItem}>
+                                <span>{mongodb}</span>
+                                <p>MongoDB</p>
+                            </div>
+                            <div className={styles.mainSkillItem}>
+                                <span>{photoshop}</span>
+                                <p>Photoshop</p>
+                            </div>
+                        </div>
+                    </div>
             <div className="w-full py-12 mx-auto portfolio">
                 <h2 className="text-d-h2">Portfolio</h2>
                 <div className="grid grid-cols-3 mx-auto justify-items-center">
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head';
-import { ethers, githubActions, gql, gsap, hardhat, jwt, mongodb, nextjs, node, photoshop, python, solitidy, tailwind, typescript } from '../components/Icons';
 import { BaseLayout } from '../Layouts/Base';
-import styles from '../styles/About.module.css'
 
 const About: NextPage = () => {
     return (
@@ -25,70 +23,7 @@ const About: NextPage = () => {
                         <p>Apart from Web3, my current interests are: Machine Learning & AI,
                             Robotics, Internet of Things and the Mixed Reality umbrella. I&apos;ve already developed in ML & AI before, but my goal is to eventually get deep in my dev bag and learn these themes thoroughly.</p>
                     </div>
-                    <div className="py-12">
-                        <h2 className="text-m-h2 sm:text-d-h2"></h2>
-                        <div className="grid grid-cols-2 gap-4 mt-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-y-8 justify-items-center">
-                            <div className={styles.mainSkillItem}>
-                                <span>{nextjs}</span>
-                                <p>NextJS</p>
-                            </div>
-                            <div className={styles.mainSkillItem}>
-                                <span>{typescript}</span>
-                                <p>Typescript</p>
-                            </div>
-                            <div className={styles.mainSkillItem}>
-                                <span>{tailwind}</span>
-                                <p>Tailwind</p>
-                            </div>
-                            <div className={styles.mainSkillItem}>
-                                <span>{node}</span>
-                                <p>Node & Express</p>
-                            </div>
-
-                            <div className={styles.mainSkillItem}>
-                                <span>{gql}</span>
-                                <p>GraphQL</p>
-                            </div>
-                            <div className={styles.mainSkillItem}>
-                                <span>{jwt}</span>
-                                <p>JWT</p>
-                            </div>
-                            <div className={`${styles.mainSkillItem} relative`}>
-                                <span>{gsap}</span>
-                                <p>GSAP</p>
-                            </div>
-                            <div className={styles.mainSkillItem}>
-                                <span>{githubActions}</span>
-                                <p>GitHub Actions</p>
-                            </div>
-                            <div className={styles.mainSkillItem}>
-                                <span>{python}</span>
-                                <p>Python</p>
-                            </div>
-                            <div className={styles.mainSkillItem}>
-                                {/* <SiEthereum className='duration-300 w-14 h-14 fill-azure-500' /> */}
-                                <span>{ethers}</span>
-                                <p>EthersJS</p>
-                            </div>
-                            <div className={styles.mainSkillItem}>
-                                {/* <FaHardHat className='duration-300 w-14 h-14 fill-jasper-500' /> */}
-                                <span>{ hardhat}</span>
-                                <p>Hardhat</p>
-                            </div>
-                            <div className={styles.mainSkillItem}>
-                                {solitidy}
-                                <p>Solidity</p>
-                            </div>
-                            <div className={styles.mainSkillItem}>
-                                <span>{mongodb}</span>
-                                <p>MongoDB</p>
-                            </div>
-                            <div className={styles.mainSkillItem}>
-                                <span>{photoshop}</span>
-                                <p>Photoshop</p>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </div>
             </BaseLayout>
         </>
