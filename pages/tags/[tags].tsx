a42809dcf6b6a82f7c4f9f0977e5363ac92a8ee1
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-16 13:14:37 +0100

    added Head tag to /tags slug file

--- pages/tags/[tags].tsx
+++ pages/tags/[tags].tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from 'next'
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 const Tags: NextPage = () => {
     const router = useRouter()
     const {tags} = router.query
     return (
-        <div className='h-screen text-sylver-100'>this is a page { tags}</div>
+        <>
+            <Head>
+                
+            </Head>
+             <div className='h-screen text-sylver-100'>this is a page { tags}</div>
+        </>
     )
 };
 
