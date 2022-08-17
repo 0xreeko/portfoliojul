5e823f8f93d0eb87ef0688e893d0a4996740ce5c
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-17 01:35:18 +0100

    added rel='noreferrer' to social links in Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -24,10 +24,10 @@ export const Header = () => {
             {router.route === '/' ? <p className='cursor-pointer select-none'>0xreeko</p> : <Link href={'/'}>0xreeko</Link>}
             <div className={styles.headerNavigation}>
                 <div className="flex items-center gap-4 pr-6">
-                    <a href="https://twitter.com/intent/follow?screen_name=0xreeko" target={`_blank`}><span className='duration-300 nav hover:text-amethyst-400'>{twitterIcon}</span></a>
-                    <a href="https://discord.com/users/0xreeko#1744" target={`_blank`}><span className='duration-300 nav hover:text-amethyst-400'>{discordIcon}</span></a>
-                    <a href="https://www.linkedin.com/in/enrictrillo/" target={`_blank`}><span className='duration-300 nav hover:text-amethyst-400'>{linkedInIcon}</span></a>
-                    <a href="https://www.youtube.com/channel/UCov6heLQcDoSUD2rwhyGdUA/" target={'_blank'}><span className='duration-300 nav hover:text-amethyst-400'>{youtubeIcon}</span></a>
+                    <a href="https://twitter.com/intent/follow?screen_name=0xreeko" target={`_blank`} rel="noreferrer"><span className='duration-300 nav hover:text-amethyst-400'>{twitterIcon}</span></a>
+                    <a href="https://discord.com/users/0xreeko#1744" target={`_blank`} rel="noreferrer"><span className='duration-300 nav hover:text-amethyst-400'>{discordIcon}</span></a>
+                    <a href="https://www.linkedin.com/in/enrictrillo/" target={`_blank`} rel="noreferrer"><span className='duration-300 nav hover:text-amethyst-400'>{linkedInIcon}</span></a>
+                    <a href="https://www.youtube.com/channel/UCov6heLQcDoSUD2rwhyGdUA/" target={'_blank'} rel="noreferrer"><span className='duration-300 nav hover:text-amethyst-400'>{youtubeIcon}</span></a>
                 </div>
                 <div className='relative group'>
                     <Link href={'/about'}>Initiatives </Link>
