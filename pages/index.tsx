3a4d70ea79d220aa2719008103d6606b7fcecc4b
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-19 09:31:28 +0100

    missed alert and header comps in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { Alert } from '../components/Alert/Alert'
 import { Footer } from '../components/Footer/Footer'
+import { Header } from '../components/Header/Header'
 import { Main } from '../components/Main/Main'
 import styles from '../styles/BaseLayout.module.css'
 
@@ -13,6 +15,8 @@ const Home: NextPage = () => {
         <meta name="description" content="The Coolest Fullstack Web3 Developer Building Cool Sh*t In Public." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Alert />
+      <Header />
       <div className={styles.wrapper}>
         <main className={styles.main}>
           <Main />
