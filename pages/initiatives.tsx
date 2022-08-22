e8d25bb752a87c110e7a6daa9fab5c84ae585d4e
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-22 11:13:10 +0100

    added meta img

--- pages/initiatives.tsx
+++ pages/initiatives.tsx
@@ -1,16 +1,25 @@
 import type { NextPage } from 'next'
 import Head from 'next/head';
+import React from 'react';
 import { Alert } from '../components/Alert/Alert';
 import { Footer } from '../components/Footer/Footer';
 import { Header } from '../components/Header/Header';
 import styles from '../styles/BaseLayout.module.css'
 
-const InitCard = () => {
+interface Props {
+    id: number,
+    name: string,
+    status: string,
+    description: string
+}
+
+const InitCard = (attributes: Props) => {
     return (
-        <div className="flex flex-col w-full p-4 duration-300 border rounded-lg text-d-base border-sylver-100 h-44 bg-amethyst-400/5 hover:bg-amethyst-400/10 border-opacity-5 hover:border-opacity-10">
-            <p className='text-m-sub2 md:text-d-sub2'>Initiative: #1</p>
-            <p className='mt-4'>Status: <span className='text-sylver-800'>Not Started</span></p>
-            <p>Description: <span className='text-sylver-800'>An API powered by X for Y and Z. </span></p>
+        <div className="flex flex-col w-full p-4 duration-300 border rounded-lg h-fit text-d-base border-sylver-100 bg-amethyst-400/5 hover:bg-amethyst-400/10 border-opacity-5 hover:border-opacity-10">
+            <p className='text-m-sub2 md:text-d-sub2'>Initiative: #{attributes.id}</p>
+            <p className='mt-4'>Name: <span className={`text-sylver-800`}>{attributes.name}</span></p>
+            <p>Status: <span className={`${attributes.status === "Not Started" ? "text-sylver-800" : "text-green-600"}`}>{attributes.status}</span></p>
+            <p>Description: <span className='text-sylver-800'>{attributes.description}</span></p>
         </div>
     )
 }
@@ -26,25 +35,18 @@ const Initiatives: NextPage = () => {
             <Header />
             <div className={styles.wrapper}>
                 <main className={styles.main}>
-                    <div className="relative flex flex-col items-center">
+                    <div className="">
                         <h1 className='mt-12 font-bold text-m-h1 md:text-d-h1'>0xreeko&apos;s Initiatives</h1>
                         <div className='w-full mt-12'>
                             {/* <div className="">SECTION TO INCLUDE ALL COMBINED STATS ex. MRR</div> */}
                             <div className="grid w-full grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-3 justify-items-center">
-                                <InitCard />
-                                <InitCard />
-                                <InitCard />
-                                <InitCard />
-                                <InitCard />
-                                <InitCard />
-                                <InitCard />
-                                <InitCard />
-                                <InitCard />
-                                <InitCard />
-                                <InitCard />
-                                <InitCard />
-                                <InitCard />
-                                <InitCard />
+                                {
+                                    [{ id: 1, name: "RuskelUI", status: "Started", description: "An UI library for rapid Web3 development" }].map((item, idx) => (
+                                        <>
+                                            <InitCard key={idx} id={item.id} name={item.name} status={item.status} description={item.description} />
+                                        </> 
+                                    ))
+                                }
                             </div>
                         </div>
                     </div>
