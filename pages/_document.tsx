0367e5496183338a72d862e4b388576b0cf116e2
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-22 12:29:41 +0100

    relocated metas to _doc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,36 @@ class MyDoc extends Document {
         return (
             <Html lang="en">
                 <Head>
+                    <link rel="icon" href="/favicon.ico" />
+
+                    {/* <!-- HTML Meta Tags --> */}
+                    <title>0xreeko &middot; Fullstack Web3 Developer</title>
+                    <meta name="description" content="The Coolest Fullstack Web3 Developer Building Cool Sh*t In Public." />
+                    <meta name="keywords" content="web3, 0xreeko" />
+                    <meta name="author" content="0xreeko" />
+
+                    {/* <!-- Facebook Meta Tags --> */}
+                    <meta property="og:url" content="https://portfoliojul.vercel.app/" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:title" content="0xreeko &middot; Fullstack Web3 Developer" />
+                    <meta property="og:description" content="The Coolest Fullstack Web3 Developer Building Cool Sh*t In Public." />
+                    <meta property="og:image" content="/ogmain.png" />
+                    <meta property="og:image:secure_url" content="/ogmain.png" />
+                    <meta property="og:image:type" content="image/png" />
+                    <meta property="og:image:width" content="1200" />
+                    <meta property="og:image:height" content="630" />
+
+                    {/* <!-- Twitter Meta Tags --> */}
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta property="twitter:domain" content="portfoliojul.vercel.app" />
+                    <meta property="twitter:url" content="https://portfoliojul.vercel.app/" />
+                    <meta name="twitter:title" content="0xreeko &middot; Fullstack Web3 Developer" />
+                    <meta name="twitter:description" content="The Coolest Fullstack Web3 Developer Building Cool Sh*t In Public." />
+                    <meta name="twitter:image" content="/ogmain.png" />
+                    <meta property="twitter:image:secure_url" content="/ogmain.png" />
+                    <meta property="twitter:image:type" content="image/png" />
+                    <meta property="twitter:image:width" content="1200" />
+                    <meta property="twitter:image:height" content="630" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link
                         rel="preconnect"
