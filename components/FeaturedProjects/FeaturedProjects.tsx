365ccdd20ff7cd62ac293d6f6a2a19ca31e3ffb4
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-24 09:27:22 +0100

    added content to FP

--- components/FeaturedProjects/FeaturedProjects.tsx
+++ components/FeaturedProjects/FeaturedProjects.tsx
@@ -1,7 +1,21 @@
+import { LastestCard } from '../LastestCard/LatestCard';
 import styles from './FeaturedProjects.module.css'
- 
+
 export const FeaturedProjects = () => {
     return (
-        <div className=''>this is FeaturedProjects</div>
+        <>
+            <div className="w-full py-12 mx-auto portfolio">
+                <h2 className="text-d-h2">Portfolio</h2>
+                <LastestCard />
+            </div>
+            <div className="w-full py-12 mx-auto portfolio">
+                <h2 className="text-d-h2">Portfolio</h2>
+                <div className="grid grid-cols-3 mx-auto justify-items-center">
+                    <p>hey</p>
+                    <p>hey</p>
+                    <p>hey</p>
+                </div>
+            </div>
+        </>
     )
 };
