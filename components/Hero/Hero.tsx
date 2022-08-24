3360068ca0e9b7c47f74b0221f625ac38998b70a
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-24 09:29:43 +0100

    transferred content from Hero to USP

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -42,18 +42,6 @@ export const Hero = () => {
                 <Link href={'/about'}><RuiButton size='base' color='amethyst'>More about me</RuiButton></Link>
                 <Link href={'/blog'} className={`border-2`}><RuiButton size='base' color='russian'>Read the blog</RuiButton></Link>
             </div>
-            
-            <div className="">
-                <h2 className="text-d-h2">Services</h2>
-                <div className="flex flex-wrap items-center justify-center gap-4 mt-4">
-                    <div className="flex flex-col justify-between h-40 p-4 w-80 rounded-2xl bg-russian-400/20">
-                        <h3>Web Development</h3>
-                        <p className='mt-2'>I use modern web tooling, like NextJs and Tailwind, to develop visually compelling fullstack applications.</p>
-                    </div>
-                    <div className="h-40 p-4 w-80 bg-russian-400/20 rounded-2xl">asdas</div>
-                    <div className="h-40 p-4 w-80 bg-russian-400/20 rounded-2xl">asdas</div>
-                </div>
-            </div>
         </>
     )
 };
--- components/USP/USP.tsx
+++ components/USP/USP.tsx
@@ -1,7 +1,17 @@
 import styles from './USP.module.css'
- 
+
 export const USP = () => {
     return (
-        <div className=''>this is USP</div>
+        <div className="">
+            <h2 className="text-d-h2">Services</h2>
+            <div className="flex flex-wrap items-center justify-center gap-4 mt-4">
+                <div className="flex flex-col justify-between h-40 p-4 w-80 rounded-2xl bg-russian-400/20">
+                    <h3>Web Development</h3>
+                    <p className='mt-2'>I use modern web tooling, like NextJs and Tailwind, to develop visually compelling fullstack applications.</p>
+                </div>
+                <div className="h-40 p-4 w-80 bg-russian-400/20 rounded-2xl">asdas</div>
+                <div className="h-40 p-4 w-80 bg-russian-400/20 rounded-2xl">asdas</div>
+            </div>
+        </div>
     )
 };
