985391655d307ecd5cccb01033869eb8fda9fa1b
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-24 13:01:52 +0100

    added content to Education, added paddingY to AboutMe

--- components/AboutMe/AboutMe.tsx
+++ components/AboutMe/AboutMe.tsx
@@ -2,7 +2,7 @@ import styles from './AboutMe.module.css'
  
 export const AboutMe = () => {
     return (
-        <div className='flex flex-col max-w-xl gap-y-6'>
+        <div className='flex flex-col max-w-xl py-12 gap-y-6'>
             <p>GM anon! I&apos;m 0xreeko, a 23 year old Fullstack Web3 Developer and I enjoy solving problems and materialising visions into tangible products. Born in Madrid, and have been living in London for the past 12 years.</p>
             <p>I started my journey off as a freelance Digital Developer back in 2014, covering: Djing, GFX/UI/UX design, Video Editing, Web development and much more. I wrote my first line of code in 2016 and niched down to web development in 2018, to use the experience I built over the years for my web projects.</p>
             <p>After graduating from UoN in 2020, I spent some months learning about the dev side of Web3, leading to transferring my skills to Web3 in Jan 2021. Since then, I&apos;ve helped many devs make the transition from Web2 to Web3. Currently, I&apos;m building RuskelUI, a component library for rapid Web3 development using React, TailwindCSS and Storybook.</p>
--- components/Education/Education.tsx
+++ components/Education/Education.tsx
@@ -2,6 +2,28 @@ import styles from './Education.module.css'
  
 export const Education = () => {
     return (
-        <div className=''>this is Education</div>
+        <div className='py-12'>
+            <h3 className="underline underline-offset-4 text-d-h3 decoration-amethyst-400">education.</h3>
+            <div className='flex flex-wrap justify-between gap-8 mt-3'>
+            <div>
+                <h3 className='font-bold text-d-sub2 text-amethyst-400'>The University Of Northampton</h3>
+                <p className='italic'>2017 - 2020</p>
+                <p>BSc Computing (Graphics & Visualisation)</p>
+                <p>Final Grade: Second Class Upper (2:1) </p>
+            </div>
+            <div>
+                <h3 className='font-bold text-d-sub2 text-amethyst-400'>Saint Francis Xavier</h3>
+                <p className='italic'>2015 - 2017</p>
+                <p>BTEC Level 3 Extended Diploma</p>
+                <p>Final Grade: DMM </p>
+            </div>
+            <div>
+                <h3 className='font-bold text-d-sub2 text-amethyst-400'>St Mary's RC High School</h3>
+                <p className='italic'>2010 - 2015</p>
+                <p>GCSE</p>
+                <p>Final Grade: 11 GCSEs (A* - C)</p>
+            </div>
+        </div>
+        </div>
     )
 };
