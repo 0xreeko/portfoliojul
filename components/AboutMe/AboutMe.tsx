9e1ec85364474d71b9ff3f212c45ef5a398f0c81
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-25 13:10:56 +0100

    activated gsap animation again. added maxWidth to aboutMe

--- components/AboutMe/AboutMe.tsx
+++ components/AboutMe/AboutMe.tsx
@@ -2,7 +2,7 @@ import styles from './AboutMe.module.css'
 
 export const AboutMe = () => {
     return (
-        <div className='flex flex-col items-start justify-between gap-6 py-12'>
+        <div className='flex flex-col items-start justify-between max-w-xl gap-6 py-12'>
             <h3 className="underline underline-offset-4 text-m-h3 md:text-d-h3 decoration-amethyst-400">aboutMe.</h3>
                 <p>I&apos;m 0xreeko, a Fullstack Web3 Developer and I enjoy solving problems and materialising visions into tangible products. Born in Madrid, and have been living in London for the past 12 years.</p>
                 <p>I started my journey off as a freelance Digital Developer back in 2014, covering: Djing, GFX/UI/UX design, Video Editing, Web development and much more. I wrote my first line of code in 2016 and niched down to web development in 2018, to use the experience I built over the years for my web projects.</p>
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -9,16 +9,16 @@ export const Hero = () => {
     let punkLogo = useRef(null)
     let hero = useRef(null)
 
-    // useEffect(() => {
-    //     gsap.fromTo(punkLogo, {
-    //         y: -45,
-    //         opacity: 0,
-    //     }, { y: 0, delay: .9, duration: 5, opacity: 1, ease: 'elastic' })
-    //     gsap.fromTo(hero, {
-    //         y: -45,
-    //         opacity: 0,
-    //     }, { y: 0, delay: .3, duration: 5, opacity: 1, ease: 'elastic' })
-    // }, [])
+    useEffect(() => {
+        gsap.fromTo(punkLogo, {
+            y: -45,
+            opacity: 0,
+        }, { y: 0, delay: .9, duration: 5, opacity: 1, ease: 'elastic' })
+        gsap.fromTo(hero, {
+            y: -45,
+            opacity: 0,
+        }, { y: 0, delay: .3, duration: 5, opacity: 1, ease: 'elastic' })
+    }, [])
 
     return (
         <>
@@ -29,12 +29,11 @@ export const Hero = () => {
                     <h4 className='text-center sm:text-left text-m-h4 sm:text-d-h4'>GM anon👾, I&apos;m </h4>
                     <h1 className='tracking-widest text-center sm:text-left text-m-hero sm:text-d-hero text-amethyst-400 hover:text-amethyst-400/20'>0xreeko</h1>
                     <h5 className='text-center sm:text-left text-m-sub1 sm:text-d-sub1'>Fullstack Web3 Developer</h5>
-                    {/* <h2 className='max-w-full font-medium text-center sm:text-left text-m-h2 md:text-d-h2'>materialising visions into <span className='text-amethyst-500'>tangible products</span></h2> */}
                     <p className='max-w-lg mt-4 text-center sm:text-left text-m-base md:text-d-base'>I'm a creative based in London and I enjoy solving problems and materialising visions into <span className='italic text-amethyst-400'>tangible products</span>. Currently, I'm building an UI library for rapid Web3 development at <span className='text-amethyst-400'>RuskelUI</span>.</p>
                 </div>
 
                 {/* @ts-ignore */}
-                <div className='relative inline-block p-2 rounded-full bg-gradient-to-tr from-amethyst-400/60 to-transparent w-fit'>
+                <div className='relative inline-block p-2 rounded-full bg-gradient-to-tr from-amethyst-400/60 to-transparent w-fit' ref={el => punkLogo = el}>
                     <div className="flex p-2 rounded-full bg-russian-600">
                         <Image width={172} height={172} layout={'fixed'} placeholder="blur" src={brandIcon} alt="0xreeko NFT headshot" className="rounded-full bg-amethyst-500" />
                     </div>
