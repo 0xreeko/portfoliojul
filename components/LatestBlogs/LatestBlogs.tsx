d51f0886ac15689d7cee7d23bddca46480fc3e49
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-25 20:54:15 +0100

    modded title with section styling

--- components/LatestBlogs/LatestBlogs.tsx
+++ components/LatestBlogs/LatestBlogs.tsx
@@ -1,7 +1,7 @@
 export const LatestBlogs = () => {
     return (
         <div className='py-12'>
-            <h3 className='text-m-h3 md:text-d-h3'>latestBlogs.</h3>
+            <h3 className='underline underline-offset-4 text-m-h3 md:text-d-h3 decoration-amethyst-400'>latestBlogs.</h3>
             <ol className='relative flex flex-col w-full gap-4 mt-12'>
                 {/* map and slice by 3  */}
             </ol>
