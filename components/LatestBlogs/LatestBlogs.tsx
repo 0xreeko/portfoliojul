c6ee921fb609dc4d9c50f4e74a53898748d3c8d9
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-26 16:06:32 +0100

    added a key to mapped HoriCards

--- components/LatestBlogs/LatestBlogs.tsx
+++ components/LatestBlogs/LatestBlogs.tsx
@@ -11,8 +11,8 @@ export const LatestBlogs = ({ props }: Props) => {
             <h3 className='underline underline-offset-4 text-m-h3 md:text-d-h3 decoration-amethyst-400'>latestBlogs.</h3>
             <ol className='relative flex flex-col w-full gap-4 mt-12'>
                 {
-                    props.map((item) => (
-                        <HoriCard title={item.title} slug={item.slug} date={item.date} description={item.description} tags={item.tags} />
+                    props.map((item, idx) => (
+                        <HoriCard key={idx} title={item.title} slug={item.slug} date={item.date} description={item.description} tags={item.tags} />
                     )).slice(0, 2)
                 }
             </ol>
