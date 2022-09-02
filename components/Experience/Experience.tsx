99c7406e73bc62f591515e34072583fb81651dc4
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-09-02 22:06:09 +0100

    modded type of startYear and endYear from str to int, populated the ExpCard with require props

--- components/Experience/Experience.tsx
+++ components/Experience/Experience.tsx
@@ -4,23 +4,23 @@ interface Props {
     title: string
     company: string
     startMonth: string
-    startYear: string
+    startYear: number
     endMonth: string
-    endYear: string
+    endYear: number
 }
 
 const _data: Props[] = [
-    
+    { title: "Web3 Contractor", company: 'CWJ Capital', startMonth: "Mar", startYear: 2022, endMonth: "Jul", endYear: 2022 },
 ]
 
-const ExpCard = () => (
+const ExpCard = ({ title, company, startMonth, startYear, endMonth, endYear }: Props) => (
     <div className="p-4 duration-300 border rounded-lg backdrop-blur-sm bg-amethyst-400/5 hover:bg-amethyst-400/10 border-sylver-100 border-opacity-5 hover:border-opacity-10">
-        <h4 className='text-m-sub1 md:text-d-sub1'><span className='text-sylver-100'>role </span><span className='text-amethyst-400'>@ company</span></h4>
-        <p className='font-mono text-d-copy'>July - December 2021</p>
+        <h4 className='text-m-sub1 md:text-d-sub1'><span className='text-sylver-100'>{title} </span><span className='text-amethyst-400'>@ {company}</span></h4>
+        <p className='font-mono text-d-copy'>{startMonth} {startYear !== endYear ? startYear : '' } - {endMonth} {endYear}</p>
         <p className='text-d-base'>ABSC</p>
     </div>
 )
- 
+
 export const Experience = () => {
     return (
         <section id="experience" className='py-12'>
@@ -30,12 +30,11 @@ export const Experience = () => {
                 <span className='h-[1px] w-full my-auto max-w-xs bg-sylver-800 opacity-40'></span>
             </div>
             <div className="flex flex-col w-full gap-6 mt-8">
-                <ExpCard/>
-                <ExpCard/>
-                <ExpCard/>
-                <ExpCard/>
-                <ExpCard/>
-                <ExpCard/>
+                {
+                    _data.map(({ title, company, startMonth, startYear, endMonth, endYear }, idx) => (
+                        <ExpCard key={idx} title={title} company={company} startMonth={startMonth} startYear={startYear} endMonth={endMonth} endYear={endYear} />
+                    ))
+                }
             </div>
         </section>
     )
