e3239459616a6e012a81295aa39352edb2ccc883
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-09-04 22:31:39 +0100

    modded Contact section text

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -10,8 +10,8 @@ export const Contact = () => {
                 <h3 className='underline underline-offset-4 text-m-h3 md:text-d-h3 decoration-amethyst-400'>Contact</h3>
                 <span className='h-[1px] w-full my-auto max-w-xs bg-sylver-800 opacity-40'></span>
             </div>
-                <p className='mt-8'>Want to connect or drop me a message about anything Web3? Complete this form and I'll do my best to get back to you ASAP!</p>
-            <div className="flex flex-col-reverse w-full gap-12 mt-8 mb-24 lg:flex-row dark">
+            <p className='max-w-lg mt-8'>Although I&apos;m building my own Web3 projects, my inbox is open for opportunities in the Web3 space. Whether you have a question or just want to say GM, I&apos;ll do my best to get back to you ASAP!</p>
+            <div className="flex flex-col-reverse flex-wrap w-full gap-12 mt-8 mb-24 lg:flex-row dark">
                 <div className="flex flex-col w-full max-w-lg gap-9">
                     <RuiInput name="name" floatingLabel='Name' color='amethyst' fullWidth />
                     <RuiInput name="subject" color='amethyst' floatingLabel='Email' fullWidth />
@@ -32,8 +32,8 @@ export const Contact = () => {
                         </div>
                     </a>
                     <a href="https://linkedin.com/in/enrictrillo" target={'_blank'}>
-                    <div className={`${styles.wrapperItem} group hover:cursor-pointer`}>
-                        <p className='duration-300 group-hover:text-transparent bg-clip-text bg-gradient-to-r from-[#0077b5] to-[#00A0DC]'>Connect w/ me on LinkedIn</p>
+                        <div className={`${styles.wrapperItem} group hover:cursor-pointer`}>
+                            <p className='duration-300 group-hover:text-transparent bg-clip-text bg-gradient-to-r from-[#0077b5] to-[#00A0DC]'>Connect w/ me on LinkedIn</p>
                             <span className='text-sylver-100 duration-300 group-hover:text-[#00A0DC]'>{linkedInIcon}</span>
                         </div>
                     </a>
