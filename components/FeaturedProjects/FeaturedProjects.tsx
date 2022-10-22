6708077c9d8a9dab3d5e14d8d11bbbb448c83ac6
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-10-22 23:33:21 +0100

    added mx-8 to Featured Projects

--- components/FeaturedProjects/FeaturedProjects.tsx
+++ components/FeaturedProjects/FeaturedProjects.tsx
@@ -30,7 +30,7 @@ const FeaturedCard = (props: FPProps) => {
 
 export const FeaturedProjects = ({ props }: Props) => {
     return (
-        <section id="portfolio" className="w-full py-16 mx-auto ">
+        <section id="portfolio" className="py-16 mx-8">
             <div className="flex items-baseline gap-4">
                 <span className='font-mono text-m-sub1 md:text-d-sub1 text-amethyst-400'>5.0</span>
                 <h3 className="underline underline-offset-4 text-m-h3 md:text-d-h3 decoration-amethyst-400 whitespace-nowrap">Featured Projects</h3>
