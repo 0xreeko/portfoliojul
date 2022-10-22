d066c6efce8621c8c1bb1b0c7c0e07b1f1878d2e
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-10-23 00:26:58 +0100

    fixed color type import from ruskel and updated the source, since it's in generics now

--- @types/tags.ts
+++ @types/tags.ts
@@ -1,8 +1,8 @@
-import { colorType } from "ruskelui/dist/utils"
+import { color } from "ruskelui/dist/types/Generics"
 
 export const tagsType = ['Databases', 'Security', 'Reactjs', 'Robotics', 'Testing', 'Python', 'Web3', 'GraphQL', 'TailwindCSS3', 'Nextjs', 'APIs', 'Typescript', 'GSAP']
 
-export const variant: { [key in string]: colorType } = {
+export const variant: { [key in string]: color } = {
     'Databases': 'green',
     'Security': 'cyan',
     'Reactjs': 'jasper',
