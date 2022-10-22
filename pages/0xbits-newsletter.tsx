71dd8247e944d0f6c914d09d3e064a6c69196954
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-10-22 23:27:29 +0100

    added styles gradient styles to subscribe section in Hero, new page that'll act as LP for my newsletter

---
+++ pages/0xbits-newsletter.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Header } from '../components/Header/Header';
+
+const Subscribe: NextPage = () => {
+    return (<>
+        <Header/>
+        <div className='h-screen'>this is a landing page for 0xbits</div>
+    </>
+    )
+};
+
+export default Subscribe
