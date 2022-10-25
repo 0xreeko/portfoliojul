7d4b8f54962e820c774629d336b3ada9f21193a1
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-10-25 12:54:29 +0100

    added env var for NextGA

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
       's3.us-west-2.amazonaws.com',
       '/'
     ]
+  },
+  env: {
+    NEXT_GA: process.env.NEXT_GA
   }
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { gaMeasurementId } from '../constants/'
 import NextGA from '../components/NextGA'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
-            <NextGA mId={gaMeasurementId} />
+            <NextGA mId={`${process.env.NEXT_GA}`} />
             <Component {...pageProps} />
         </>
     )
