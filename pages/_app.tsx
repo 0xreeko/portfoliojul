2d1cfd20843cfe03215f7f16d13c704667dfba1c
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-10-25 12:47:00 +0100

    fixing err

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import {gaMeasurementId} from '../constants'
+import { gaMeasurementId } from '../constants/'
 import NextGA from '../components/NextGA'
 
 function MyApp({ Component, pageProps }: AppProps) {
