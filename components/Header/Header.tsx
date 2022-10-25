415914fdb22a36cd9fa12cbfc09e0975d6cf48a2
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-10-25 12:13:57 +0100

    added custom GA component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -12,42 +12,42 @@ export const Header = () => {
     return (
         <header className={styles.headerContainer}>
             <nav className={styles.nav}>
-            <div className="flex items-center">
-                {
-                    router.route === '/' ?
-                        (
-                            <Image src={'/punk4596.png'} layout={'fixed'} width={32} height={32 } className={'-translate-y-0.5'} alt={'smaller 0xreeko NFT headshot'} />
-                        ) : (
-                            <a href={'/'}>
-                                <Image src={'/punk4596.png'} layout={'fixed'} width={32} height={32} className='hover:cursor-pointer -translate-y-0.5' alt={'smaller 0xreeko NFT headshot'} />
-                            </a>
-                        )
-                }
-            </div>
-            <div className={styles.headerNavigation}>
-                <div className='relative group'>
-                    <Link href={'/initiatives'}>Initiatives </Link>
-                    <div className={`absolute -bottom-2 rounded-lg h-[1px] ${router.route.split('/')[1] === "initiatives" ? "w-full bg-amethyst-500" : "w-[0%] bg-transparent group-hover:w-[100%] group-hover:bg-amethyst-500"} duration-300`}></div>
+                <div className="flex items-center">
+                    {
+                        router.route === '/' ?
+                            (
+                                <Image src={'/punk4596.png'} layout={'fixed'} width={32} height={32} className={'-translate-y-0.5'} alt={'smaller 0xreeko NFT headshot'} />
+                            ) : (
+                                <a href={'/'}>
+                                    <Image src={'/punk4596.png'} layout={'fixed'} width={32} height={32} className='hover:cursor-pointer -translate-y-0.5' alt={'smaller 0xreeko NFT headshot'} />
+                                </a>
+                            )
+                    }
                 </div>
-                <div className='relative group'>
-                    <Link href={'/about'}>About </Link>
-                    <div className={`absolute -bottom-2 rounded-lg h-[1px] ${router.route.split('/')[1] === "about" ? "w-full bg-amethyst-500" : "w-[0%] bg-transparent group-hover:w-[100%] group-hover:bg-amethyst-500"} duration-300`}></div>
-                </div>
-                <div className='relative group'>
-                    <Link href={'/blog'}>Blog</Link>
-                    <div className={`absolute -bottom-2 rounded-lg h-[1px] ${router.route.split('/')[1] === "blog" ? "w-[100%] bg-amethyst-500" : "w-[0%] bg-transparent group-hover:w-[100%] group-hover:bg-amethyst-500"} duration-300`}></div>
-                </div>
-                    { router.route.split('/')[1] !== 'newsletter' && 
+                <div className={styles.headerNavigation}>
+                    <div className='relative group'>
+                        <Link href={'/initiatives'}>Initiatives </Link>
+                        <div className={`absolute -bottom-2 rounded-lg h-[1px] ${router.route.split('/')[1] === "initiatives" ? "w-full bg-amethyst-500" : "w-[0%] bg-transparent group-hover:w-[100%] group-hover:bg-amethyst-500"} duration-300`}></div>
+                    </div>
+                    <div className='relative group'>
+                        <Link href={'/about'}>About </Link>
+                        <div className={`absolute -bottom-2 rounded-lg h-[1px] ${router.route.split('/')[1] === "about" ? "w-full bg-amethyst-500" : "w-[0%] bg-transparent group-hover:w-[100%] group-hover:bg-amethyst-500"} duration-300`}></div>
+                    </div>
+                    <div className='relative group'>
+                        <Link href={'/blog'}>Blog</Link>
+                        <div className={`absolute -bottom-2 rounded-lg h-[1px] ${router.route.split('/')[1] === "blog" ? "w-[100%] bg-amethyst-500" : "w-[0%] bg-transparent group-hover:w-[100%] group-hover:bg-amethyst-500"} duration-300`}></div>
+                    </div>
+                    {router.route.split('/')[1] !== 'newsletter' &&
                         <a href={'/newsletter'}>
-                        <RuiButton color='amethyst' variant='filled' size='sm'>
-                            <span className='font-medium tracking-wide'>Join the Woze newsletter</span>
-                        </RuiButton>
-                    </a>
-                }
-            </div>
-            <div className={styles.headerMenu}>
-                {menu}
-            </div>
+                            <RuiButton color='amethyst' variant='filled' size='sm'>
+                                <span className='font-medium tracking-wide'>Join the Woze newsletter</span>
+                            </RuiButton>
+                        </a>
+                    }
+                </div>
+                <div className={styles.headerMenu}>
+                    {menu}
+                </div>
             </nav>
         </header>
     )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
 import {gaMeasurementId} from '../constants'
+import NextGA from '../components/NextGA'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
-            <Script strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`} />
-            <Script id='google-analytics' strategy='afterInteractive'>
-                {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-              
-                gtag('config', '${gaMeasurementId}');
-                `}
-            </Script>
+            <NextGA mId={gaMeasurementId} />
             <Component {...pageProps} />
         </>
     )
