69331f8a5a45cbbe2974c84bb8ced4d510112e1a
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-10-29 15:46:34 +0100

    added nextgas I just created to handle ga script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import NextGA from '../components/NextGA'
+import { NextGA} from 'nextgas'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
